1dcc636a741c20fa6d2ebed5dd1d499a5aaf4c63
jungcolor <jungcolor@gmail.com>
2023-05-28 14:53:52 +0900

    Edu: Component State

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Header(props) {
     const { title, onChangeMode } = props;
     const onClickHandler = (e) => {
@@ -18,7 +20,7 @@ function Nav(props) {
         const { target } = e;
 
         e.preventDefault();
-        onChangeMode(target.id);
+        onChangeMode(Number(target.id));
     };
 
     return (
@@ -50,21 +52,36 @@ function Article(props) {
 }
 
 function App() {
+    const [mode, setMode] = useState("WELCOME");
+    const [id, setId] = useState(null);
     const topics = [
         { id: 1, title: "html", body: "html is ..." },
         { id: 2, title: "css", body: "css is ..." },
         { id: 3, title: "javascript", body: "javascript is ..." }
     ];
+    let content = null;
+
+    if (mode === "WELCOME") {
+        content = <Article title="Welcome" body="Hello, WEB"></Article>;
+    }
+    else if (mode === "READ") {
+        topics.forEach(topic => {
+            if (topic.id === id) {
+                content = <Article title={topic.title} body={topic.body}></Article>
+            }
+        })
+    }
 
     return (
         <div>
             <Header title="WEB" onChangeMode={() => {
-                alert("header");
+                setMode("WELCOME");
             }}></Header>
-            <Nav topics={topics} onChangeMode={id => {
-                alert(id);
+            <Nav topics={topics} onChangeMode={_id => {
+                setMode("READ");
+                setId(_id);
             }}></Nav>
-            <Article title="Welcome" body="Hello, WEB"></Article>
+            {content}
         </div>
     );
 }
