12a75457f710c9aa134bf1eefebd7245d9dfc840
reon <reon@cookat.co>
2023-10-10 11:28:59 +0900

    REMOVE: File Reset

--- basic-react/README.old.md
+++
@@ -1,7 +0,0 @@
-# On_My_Own
-
-만들어보고 싶거나 해보고 싶었던거 내 마음대로 해보려고
-
-# 기술 Spec (하나씩 추가할 예정)
-
--   React (create-react-app)
--- basic-webpack/src/index.js
+++
@@ -1,9 +0,0 @@
-import './reset.css';
-
-window.addEventListener("DOMContentLoaded", (e) => {
-    const app = document.getElementById("app");
-    app.innerText = "webpack settings";
-    // console.log(process.env.ENV_TEST);
-
-    document.body.append(app);
-});
