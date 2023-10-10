0854f2d16f88258cbf6ee16e51afa1f0ff27aace
reon <reon@cookat.co>
2023-10-10 14:47:33 +0900

    Feat: Entry Point 추가

---
+++ timer/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <h1>시작시작</h1>;
+};
+
+export default App;
---
+++ timer/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import './styles/tailwind.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+);
